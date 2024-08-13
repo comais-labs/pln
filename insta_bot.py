66004e4d3c2d33613e33fe10e741434549623504
Rogério Nogueira <roger@uft.edu.br>
2024-08-13 12:14:48 -0300

    Update insta_bot.py

--- insta_bot.py
+++ insta_bot.py
@@ -7,10 +7,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 PASSWORD = os.environ.get('PASSWORD')
 
 driver = webdriver.Chrome()
+driver.implicitly_wait(10)
 driver.get('https://www.instagram.com/')
 time.sleep(5)
 
@@ -20,33 +20,57 @@ username.send_keys('roger_nogueira')
 password.send_keys(PASSWORD)
 password.send_keys(Keys.RETURN)
 time.sleep(5)
-driver.get('https://www.instagram.com/ppggtd_uft/')
+driver.get('https://www.instagram.com/illuminainc/')
 time.sleep(2)
-postsposts_elements = driver.find_elements(By.CLASS_NAME, '_aagw')
-#posts_elements = driver.find_elements(By.XPATH, '//a[contains(@href, "/p/")]')
+#postsposts_elements = driver.find_elements(By.CLASS_NAME, '_aagw')
+posts_elements = driver.find_elements(By.XPATH, '//a[contains(@href, "/p/") or contains(@href, "/reel/")]')
 
+posts_urls = []
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    time.sleep(5)
-    #new_posts = driver.find_elements(By.XPATH, '//a[contains(@href, "/p/")]')
-    new_posts = driver.find_elements(By.CLASS_NAME, '_aagw')
+    time.sleep(10)
+    new_posts = driver.find_elements(By.XPATH, '//a[contains(@href, "/p/") or contains(@href, "/reel/")]')
+    #new_posts = driver.find_elements(By.CLASS_NAME, '_aagw')
 
     if len(new_posts) == len(posts_elements):
         break
+    if len(posts_elements) >= 20:
+        break
     posts_elements = new_posts
+    posts_urls = [post.get_attribute('href') for post in posts_elements]
     
 posts = posts_elements
-for post in posts:
-    post.click()
-    time.sleep(2)
+dict_posts = []
+for post in posts_urls:
+    #post.click()
+    driver.get(post)
+    time.sleep(3)
     ele_comentarios = driver.find_elements(By.CLASS_NAME, '_a9zo')
-    for comentario in ele_comentarios[1:]:
-        autor = comentario.find_element(By.TAG_NAME, 'h3').text
-        texto = comentario.find_element(By.CLASS_NAME, '_a9zs').text
-        print('Autor:', autor)
-        print('Comentario:', texto)
-    close = driver.find_element(By.CLASS_NAME,'x160vmok')
-    close.click()
+    if len(ele_comentarios) > 1:
+        comentarios = []    
+        for comentario in ele_comentarios[1:]:
+            autor = comentario.find_element(By.TAG_NAME, 'h3').text
+            texto = comentario.find_element(By.CLASS_NAME, '_a9zs').text
+            print('Autor:', autor)
+            print('Comentario:', texto)
+            comentarios.append({'autor': autor, 'comentario': texto})
+    else:
+        comentarios = []
+    dict_posts.append({
+        'url':post, 
+        'comentarios': comentarios
+    })
+    #close = driver.find_element(By.CLASS_NAME,'x160vmok')
+    #close.click()
     time.sleep(2)
+
+
+row=[]
+for post in dict_posts:
+    for comentario in post['comentarios']:
+        row.append({'url':post['url'], 'autor':comentario['autor'], 'comentario':comentario['comentario']})
+
+df = pd.DataFrame(row)
+df.to_csv('comentarios.csv', index=False)
 
     
